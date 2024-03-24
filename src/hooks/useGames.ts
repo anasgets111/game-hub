de0e1183075e7e218ddf90a43f5bee2a041c9d05
Anas Khalifa <anasgets111@gmail.com>
2024-03-24 11:55:54 +0200

    Display platforms icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,10 +2,21 @@ import { useState, useEffect } from "react";
 import apiClients from "../services/api-clients";
 import { CanceledError } from "axios";
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+  background_image: string;
+  
+
+}
+
+
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: {platform: Platform }[];
 }
 
 interface FetchGamesResponse {
