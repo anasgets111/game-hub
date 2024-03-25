f398960caac6fd5624c105332698185aec2dabbb
Anas Khalifa <anasgets111@gmail.com>
2024-03-26 01:12:39 +0200

    sort the games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -27,7 +27,7 @@ const useGames = (gameQuery: GameQuery ) =>
   useData<Game>(
     "/games",
     { params: { genres: gameQuery.genre?.id, 
-      platforms: gameQuery.genre?.id } },
+      platforms: gameQuery.genre?.id, ordering: gameQuery.sortOrder } },
     [gameQuery]
   );
 export default useGames
