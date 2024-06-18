ae7d2193910c24a5a673ab3805afd5b2ca512230
Anas Khalifa <anasgets111@gmail.com>
2024-06-18 22:09:09 +0300

    fixed env error

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const apiKey = import.meta.env.RAWG_API_KEY
+const apiKey = import.meta.env.VITE_RAWG_API_KEY
 
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
