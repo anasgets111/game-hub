48696f102e2578b639255b082c5cc339ee1bca30
Anas Khalifa <anasgets111@gmail.com>
2024-06-18 22:00:19 +0300

    correct use of api key and env file

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,10 @@
 import axios from "axios"
 
+const apiKey = import.meta.env.RAWG_API_KEY
+
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
-  params : {
-    key: '23dcf1f81eb043499a1341bbfd23b61a'
+  params: {
+    key: apiKey
   }
-  })
+})
